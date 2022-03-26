2d11789f6ad664f2144c8b5caee21ee0262859f4
DiegoMGonet <95249706+DiegoMGonet@users.noreply.github.com>
2022-03-25 21:05:52 -0600

    Versión 1.0.2 (#3)
    - Adding guid on register target

--- app/src/main/java/com/sample/neuroidapp/us/MainActivity.kt
+++ app/src/main/java/com/sample/neuroidapp/us/MainActivity.kt
@@ -2,62 +2,38 @@ package com.sample.neuroidapp.us
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
-import com.neuroid.tracker.NeuroID
+import androidx.navigation.NavController
 import com.sample.neuroidapp.us.databinding.ActivityMainBinding
-import com.sample.neuroidapp.us.extensions.getDays
+import androidx.navigation.findNavController
+import com.sample.neuroidapp.us.listeners.NIDRegisterListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    AppCompatActivity(),
+    NIDRegisterListener {
     private lateinit var binding: ActivityMainBinding
+    private fun currentNavController(): NavController =
+        findNavController(R.id.navHostFragment)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.apply {
-            val sessionId = NeuroID.getInstance().getSessionId()
-            textViewSessionId.text = sessionId
-
-            val yearList = resources.getStringArray(R.array.nid_app_array_years)
-            val monthList = resources.getStringArray(R.array.nid_app_array_months)
-
-            val adapterYear =  ArrayAdapter(
-                this@MainActivity,
-                android.R.layout.simple_dropdown_item_1line,
-                yearList
-            )
-
-            editTextBirthYear.apply {
-                setAdapter(adapterYear)
-            }
-
-            val adapterMonth =  ArrayAdapter(
-                this@MainActivity,
-                android.R.layout.simple_dropdown_item_1line,
-                monthList
-            )
-
-            editTextBirthMonth.apply {
-                setAdapter(adapterMonth)
-                setOnItemClickListener { _, _, position, _ ->
-                    editTextBirthDay.setText("")
-                    editTextBirthDay.isEnabled = true
-                    editTextBirthDay.setAdapter(getAdapter(position))
-                }
-            }
-
-            buttonAgreeAndCheck.setOnClickListener {
-                NeuroID.getInstance().formSubmit()
-            }
-        }
-
     }
 
-    private fun getAdapter(position: Int): ArrayAdapter<Int> {
-        return  ArrayAdapter(
-            this@MainActivity,
-            android.R.layout.simple_dropdown_item_1line,
-            getDays(position)
+    override fun onBackPressed() {
+        when (currentNavController().currentDestination?.id) {
+            R.id.fragmentRegisterTwo -> currentNavController().navigateUp()
+            else -> finish()
+        }
+    }
+
+    override fun goToNextScreen() {
+        currentNavController().navigate(
+            NIDRegisterFragmentOneDirections.actionGoToFragmentRegisterTwo()
         )
+    }
+
+    override fun goBackScreen() {
+        currentNavController().navigateUp()
     }
 }
